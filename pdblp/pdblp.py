01c3eb5c805c19d241e4b86f4a1d41d379da128b
Matthew Gilbert <matthew.gilbert12@gmail.com>
2018-09-10 15:40:27 -0400

    Improve docstrings

--- pdblp/pdblp.py
+++ pdblp/pdblp.py
@@ -641,20 +641,22 @@ class BCon(object):
         return data
 
     def bsrch(self, domain):
-        """This function uses the Bloomberg API to retrieve 'bsrch'
-        (Bloomberg SRCH Data) queries. Returns list of tickers.
+        """
+        This function uses the Bloomberg API to retrieve 'bsrch' (Bloomberg
+        SRCH Data) queries. Returns list of tickers.
 
         Parameters
         ----------
         domain: string
-        A character string with the name of the domain to execute.
-        It can be a user defined SRCH screen, commodity screen or
-        one of the variety of Bloomberg examples. All domains are in the format
-        <domain>:<search_name>. Example "COMDTY:NGFLOW"
+            A character string with the name of the domain to execute.
+            It can be a user defined SRCH screen, commodity screen or
+            one of the variety of Bloomberg examples. All domains are in the
+            format <domain>:<search_name>. Example "COMDTY:NGFLOW"
+
         Returns
         -------
         data: pandas.DataFrame
-        List of bloomberg tickers from the BSRCH
+            List of bloomberg tickers from the BSRCH
         """
         logger = _get_logger(self.debug)
         request = self.exrService.createRequest("ExcelGetGridRequest")
