3980305d8816fbe6d9992ac898f475cfe6eb7765
Matthew Gilbert <matthew.gilbert12@gmail.com>
2019-09-27 18:27:31 +0800

    ENH: Add tick data support

--- pdblp/pdblp.py
+++ pdblp/pdblp.py
@@ -681,6 +681,51 @@ class BCon(object):
         data = pd.DataFrame(data).set_index('time').sort_index().loc[:, flds]
         return data
 
+    def bdt(self, ticker, start_datetime, end_datetime, event_types, elms=None):
+        """
+        Get Open, High, Low, Close, Volume, and numEvents for a ticker.
+        Return pandas DataFrame
+
+        Parameters
+        ----------
+        ticker: string
+            String corresponding to ticker
+        start_datetime: string
+            UTC datetime in format YYYY-mm-ddTHH:MM:SS
+        end_datetime: string
+            UTC datetime in format YYYY-mm-ddTHH:MM:SS
+        event_type: string {TRADE, BID, ASK, BID_BEST, ASK_BEST, BEST_BID,
+                           BEST_ASK}
+            Requested data event type
+        interval: int {1... 1440}
+            Length of time bars
+        elms: list of tuples
+            List of tuples where each tuple corresponds to the other elements
+            to be set. Refer to the IntradayBarRequest section in the
+            'Services & schemas reference guide' for more info on these values
+        """
+        elms = [] if not elms else elms
+
+        # flush event queue in case previous call errored out
+        logger = _get_logger(self.debug)
+        while(self._session.tryNextEvent()):
+            pass
+
+        # Create and fill the request for the historical data
+        request = self.refDataService.createRequest('IntradayTickRequest')
+        request.set('security', ticker)
+        request.set('startDateTime', start_datetime)
+        request.set('endDateTime', end_datetime)
+        for evt in event_types:
+            request.append('eventTypes', evt)
+        for name, val in elms:
+            request.set(name, val)
+
+        logger.info('Sending Request:\n{}'.format(request))
+        # Send the request
+        self._session.sendRequest(request, identity=self._identity)
+        for msg in self._receive_events():
+
     def bsrch(self, domain):
         """
         This function uses the Bloomberg API to retrieve 'bsrch' (Bloomberg
